63e752febcbe29cd561e8ef6e05d54efd324d060
abdalimran <abdalimran@gmail.com>
2016-06-27 10:44:28 +0600

    Initial commit.

---
+++ app/src/main/java/com/github/abdalimran/simplelistview/MainActivity.java
@@ -0,0 +1,49 @@
+package com.github.abdalimran.simplelistview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView list;
+    String[] description = {
+            "This is sample description 1.",
+            "This is sample description 2.",
+            "This is sample description 3.",
+            "This is sample description 4.",
+            "This is sample description 5.",
+            "This is sample description 6.",
+            "This is sample description 7."
+    } ;
+    Integer[] imageId = {
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+            R.drawable.img6,
+            R.drawable.img7
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomList adapter = new CustomList(MainActivity.this, description, imageId);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You Clicked at " + description[+ position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+}
